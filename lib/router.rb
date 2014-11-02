629648300de1d65e94362c9c5b0eaf82fb927720
Les Aker <me@lesaker.org>
2014-11-02 11:41:36 -0500

    fix to use post

--- lib/router.rb
+++ lib/router.rb
@@ -4,10 +4,14 @@ require 'sinatra/base'
 ##
 # The real deal, yo
 class App < Sinatra::Base
-  get '/' do
+  post '/' do
     twiml = Twilio::TwiML::Response.new do |r|
       r.Message 'Hey Monkey. Thanks for the message!'
     end
     twiml.text
+  end
+
+  get '/' do
+    'You must be lost'
   end
 end
