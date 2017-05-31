17889bd334ead6a926ccf97b6c107407c9df52b2
Les Aker <me@lesaker.org>
2017-05-30 17:52:41 -0700

    use annotated string tokens

--- lib/committed/util.rb
+++ lib/committed/util.rb
@@ -6,8 +6,8 @@ module Committed
   # Common helper methods
   module Util
     MESSAGES = {
-      true => '%{user} has committed today (%{score})',
-      false => '%{user} has not committed today'
+      true => '%<user>s has committed today (%<score>s)',
+      false => '%<user>s has not committed today'
     }.freeze
 
     def check(user)
